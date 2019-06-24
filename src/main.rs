de7637b5e0b79dc5b3df1454326a4bd6705b3aca
Aleksi Backman <bddubbb@gmail.com>
2019-06-24 20:16:41 +0300

    Add another function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+// If prefixed with "_" compiler will not warn that the parameter is not used.
+// The parameter name is not mandatory, simply "_" can be used.
+fn get_world(_x: &'static str) -> &'static str {
+    return "world";
+}
+
 fn main() {
     let world = "world";
     let explicit_world: &'static str = "world";
-    println!("Hello, {}!", world);
-    println!("Hello, {}!", explicit_world);
+    println!("Hello, {}! 1", world);
+    println!("Hello, {}! 2", explicit_world);
+
+    println!("Hello, {}! 3", get_world("anything"));
 }
