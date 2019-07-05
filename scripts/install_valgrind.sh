a550d8c186588b49208dc175926738f57750dde5
Aleksi Backman <bddubbb@gmail.com>
2019-07-05 15:47:22 +0300

    Fix directory name in install script

--- scripts/install_valgrind.sh
+++ scripts/install_valgrind.sh
@@ -3,4 +3,4 @@
 set -ex
 wget https://sourceware.org/pub/valgrind/valgrind-3.15.0.tar.bz2
 tar -xvf valgrind-3.15.0.tar.bz2
-cd valgrind-3.15.0.tar.bz2 && ./configure && make && sudo make install
+cd valgrind-3.15.0 && ./configure && make && sudo make install
