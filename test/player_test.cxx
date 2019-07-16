d07f1cc69dcadd1e569ef0d59e3881ff9fb529c6
Aleksi Backman <bddubbb@gmail.com>
2019-07-16 21:48:34 +0300

    Add tests for getting name and coordinates
    Closes: #23

--- test/player_test.cxx
+++ test/player_test.cxx
@@ -1,8 +1,9 @@
 #include <climits>
+#include <string>
 
 #include "gtest/gtest.h"
 
-// TODO(#23) actual player tests here
+#include "player.h"
 
 class PlayerTest : public ::testing::Test
 {
@@ -17,7 +18,26 @@ protected:
 };
 
 // NOLINTNEXTLINE
-TEST_F(PlayerTest,DummyTest)
+TEST_F(PlayerTest,GetName)
 {
-    ASSERT_TRUE(true);
+    std::string p2_name = "Player 2";
+    constexpr int p2_x = 10;
+    constexpr int p2_y = 20;
+
+    Player player2{p2_name, p2_x, p2_y};
+
+    ASSERT_EQ(p2_name, player2.getName()); // NOLINT
+}
+
+// NOLINTNEXTLINE
+TEST_F(PlayerTest,GetCoordinates)
+{
+    std::string p2_name = "Player 2";
+    constexpr int p2_x = 10;
+    constexpr int p2_y = 20;
+
+    Player player2{p2_name, p2_x, p2_y};
+
+    ASSERT_EQ(p2_x, player2.getPosition().getX()); // NOLINT
+    ASSERT_EQ(p2_y, player2.getPosition().getY()); // NOLINT
 }
