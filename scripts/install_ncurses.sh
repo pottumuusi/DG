efe78dec9e61e4a7307b6492a51a5d7561df83ce
Aleksi Backman <bddubbb@gmail.com>
2019-07-20 11:15:42 +0300

    Download ncurses using https instead of ftp

--- scripts/install_ncurses.sh
+++ scripts/install_ncurses.sh
@@ -10,7 +10,7 @@ cd libs
 
 ncurses_version="ncurses-6.1"
 
-wget ftp://ftp.invisible-island.net/ncurses/${ncurses_version}.tar.gz
+wget https://invisible-mirror.net/archives/ncurses/${ncurses_version}.tar.gz
 tar -xvf ${ncurses_version}.tar.gz
 cd ${ncurses_version}
 ./configure --prefix=/usr/local
